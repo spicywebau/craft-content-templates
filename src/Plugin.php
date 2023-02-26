933ecadaeb973d1cf288836d1be40c38f2125586
Thomas Templeton <thomas.templeton.dev@gmail.com>
2023-02-26 13:01:46 +1100

    Plugin now installable

---
+++ src/Plugin.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace spicyweb\contenttemplates;
+
+use craft\base\Plugin as BasePlugin;
+
+/**
+ * Content Templates main plugin class.
+ *
+ * @package spicyweb\contenttemplates
+ * @author Spicy Web <plugins@spicyweb.com.au>
+ * @since 1.0.0
+ */
+class Plugin extends BasePlugin
+{
+    /**
+     * @inheritdoc
+     */
+    public function init(): void
+    {
+        parent::init();
+    }
+}
