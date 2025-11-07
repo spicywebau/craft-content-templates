4b2c20907c7bd2bf566baeecc29cc859a61a36db
Thomas Templeton <thomas.templeton.dev@gmail.com>
2025-11-07 11:19:52 +1100

    Release 2.1.0

--- src/Plugin.php
+++ src/Plugin.php
@@ -41,7 +41,7 @@ class Plugin extends BasePlugin
     /**
      * @inheritdoc
      */
-    public string $schemaVersion = '2.0.0';
+    public string $schemaVersion = '2.1.0';
 
     /**
      * @inheritdoc
